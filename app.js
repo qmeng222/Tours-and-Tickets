0dccd5d848d5685945d3a6b89ea9bb81e814565b
Qingying Meng <qmeng222@gmail.com>
2022-12-08 15:57:36 -0800

    hangled GET request from API

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-// GET:
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Tours & Tickets' });
-});
+// // GET:
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Tours & Tickets' });
+// });
 
-// POST:
-app.post('/', (req, res) => {
-  res.send('We can post to this endpoint...');
+// // POST:
+// app.post('/', (req, res) => {
+//   res.send('We can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+// console.log(tours);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours: tours },
+  });
 });
 
 const port = 3000;
