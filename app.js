bbcb9a47735d2e5500181e917a7bb8c248467e45
Qingying Meng <qmeng222@gmail.com>
2022-12-08 20:07:01 -0800

    handled POST requests

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+// use the middleware: the data from the body is added to the request object:
+app.use(express.json());
 
 // // GET:
 // app.get('/', (req, res) => {
@@ -20,6 +22,28 @@ const tours = JSON.parse(
 );
 // console.log(tours);
 
+// POST:
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      //  201 means created:
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+// GET:
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
