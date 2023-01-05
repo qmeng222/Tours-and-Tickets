b8e99be9bbb25f7cd69bc6477bd7fc4b23ef421e
Qingying Meng <qmeng222@gmail.com>
2023-01-04 21:35:25 -0800

    handled duplicate database fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,12 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateErrorDB = (err) => {
+  const value = Object.values(err.keyValue)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -47,6 +53,7 @@ module.exports = (err, req, res, next) => {
     let error = Object.assign(err);
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateErrorDB(error);
     sendErrorProd(error, res);
   }
 };
