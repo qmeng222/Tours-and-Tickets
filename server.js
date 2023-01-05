daf98e06b86948f479dfbe4e5f285eb5668fe13e
Qingying Meng <qmeng222@gmail.com>
2023-01-04 23:31:29 -0800

    dealed with uncaught exceptions

--- server.js
+++ server.js
@@ -4,6 +4,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -30,8 +36,8 @@ const server = app.listen(port, () => {
 
 // example error, wrong DATABASE_PASSWORD:
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION! 💥 Shutting down...'); // shut down application
+  console.log(err.name, err.message);
   // close the server (give the server some time to finish all all running or pending requests), and then run the callback function to immediately abort all running or pending requests:
   server.close(() => {
     process.exit(1); // 0 stands for a success, and 1 stands for uncaught exception
