8ea0186babe84f9faf6be58125a34556bab22231
Qingying Meng <qmeng222@gmail.com>
2023-01-26 15:18:22 -0800

    processed multiple images for a tour

--- controllers/userConroller.js
+++ controllers/userConroller.js
@@ -45,7 +45,7 @@ exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 }) // 90%
-    .toFile(`public/img/users/${req.file.filename}`); // req.file.filename defined above
+    .toFile(`public/img/users/${req.file.filename}`); // req.file.filename was defined above
 
   next();
 });
