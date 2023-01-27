99bf98b1b7377fc4ad578c11ac75281918071e04
Qingying Meng <qmeng222@gmail.com>
2023-01-26 17:11:15 -0800

    built a more robust email handler

--- Utils/email.js
+++ Utils/email.js
@@ -1,28 +1,54 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  // 1. create a transporter:
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    logger: true,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Jonas Schmedtmann <${process.env.EMAIL_FROM}>`;
+  }
 
-  // 2. define the email options:
-  const mailOptions = {
-    from: 'Qingying Meng <qingying@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid:
+      return 1;
+    }
 
-  // 3. send the email:
-  await transporter.sendMail(mailOptions);
+    return nodemailer.newTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      logger: true,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    // 1) render HTML based on a pug template:
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+
+    // 2) define email options:
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject, // subject: subject,
+      html, // html: html,
+      text: htmlToText.fromString(html),
+    };
+
+    // 3) create a transport and send the email:
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to Tours & Tickets!');
+  }
 };
-
-module.exports = sendEmail;
