31981f84bccc585f00f5e5c362e04a310e558d64
Qingying Meng <qmeng222@gmail.com>
2023-01-28 22:32:04 -0800

    finished the bookings API

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,10 +4,26 @@ const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
+router.use(authController.protect);
+
 router.get(
   '/checkout-session/:tourId',
-  authController.protect,
+  // authController.protect,
   bookingController.getCheckoutSession
 );
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+// only admin or lead guide can check/create bookings, or get/edit a booking:
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
