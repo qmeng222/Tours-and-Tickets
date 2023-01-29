c8a3cf685a9ed8ba57eca6000f552f63f5385311
Qingying Meng <qmeng222@gmail.com>
2023-01-28 21:23:41 -0800

    implemented the my bookings page for logged-in user

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -15,6 +15,12 @@ router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/signup', authController.isLoggedIn, viewsController.getSignupForm);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
+router.get(
+  '/my-tours',
+  bookingController.createBookingCheckout,
+  authController.protect,
+  viewsController.getMyTours
+);
 
 router.post(
   '/submit-user-data',
